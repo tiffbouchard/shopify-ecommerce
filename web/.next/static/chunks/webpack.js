dd8d175d761363ee8f5faed3827ca4c55ff13acb
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-09-03 10:22:00 -0400

    read me

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e12661916a6b8939af02";
+/******/ 	var hotCurrentHash = "b720fb22949adef754ba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
