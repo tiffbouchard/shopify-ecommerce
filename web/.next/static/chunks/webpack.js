54192c4ce7de1367ce9ad783ec1168720b6be385
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-09-03 16:10:26 -0400

    remove font swap

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "41df413f87eca9ea093a";
+/******/ 	var hotCurrentHash = "bf8432a2c3eea7c432a4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
