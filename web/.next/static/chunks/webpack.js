01e846a9669cdc85564bd8a36d935df7d0ecd121
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-09-09 11:21:54 -0400

    merge changes and style product page

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0cfd2d5345e8f9780711";
+/******/ 	var hotCurrentHash = "902936aae95e7a22cd41";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
