db746efd6fdbe3bb9f3afa554899ad026ed4254a
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2021-02-09 18:46:01 -0500

    fix layout and app import

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/styles.css";
 import "bootstrap/dist/css/bootstrap.css";
 import ShopProvider from "../context/shopContext";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function MyApp({ Component, pageProps }) {
